758a1af1522850557c0279f16d6d2d3fcf9ac6ef
Leonardo Da SIlva <leonardo_silva48@estudante.sesisenai.org.br>
2025-11-25 19:11:39 -0300

    Pagamento
    Implementação do metodo de pagamento

---
+++ src/main/java/com/tecdes/pedido/service/PixPagamento.java
@@ -0,0 +1,10 @@
+package com.tecdes.pedido.service;
+
+public class PixPagamento implements PagamentoStrategy {
+
+    @Override
+    public boolean pagar(double valor) {
+        System.out.println("Pagamento de R$ " + valor + " realizado via PIX.");
+        return true;
+    }
+}
