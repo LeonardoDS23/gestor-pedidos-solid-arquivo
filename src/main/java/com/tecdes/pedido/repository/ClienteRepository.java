49c0d70e931d1dcd525df7f74cff024d67794dda
LeonardoDS23 <leoescanordasilvaxuxa@gmail.com>
2025-12-02 19:49:23 -0300

    Ultimo
    Ultimas atualizações

--- src/main/java/com/tecdes/pedido/repository/ClienteRepository.java
+++ src/main/java/com/tecdes/pedido/repository/ClienteRepository.java
@@ -1,13 +1,16 @@
 package com.tecdes.pedido.repository;
 
+
 import com.tecdes.pedido.model.entity.Cliente;
 import java.util.Optional;
 import java.util.List;
 
+
 public interface ClienteRepository {
 
+
     Optional<Cliente> findByNome(String nome);
-    
+   
     Cliente save(Cliente cliente);
     Optional<Cliente> findById(Long id);
     List<Cliente> findAll();
--- src/main/java/com/tecdes/pedido/repository/UsuarioRepository.java
+++ src/main/java/com/tecdes/pedido/repository/UsuarioRepository.java
@@ -1,10 +1,13 @@
 package com.tecdes.pedido.repository;
 
+
 import com.tecdes.pedido.model.entity.Usuario;
 import java.util.List;
 import java.util.Optional;
 
+
 public interface UsuarioRepository {
+
 
     Usuario save(Usuario usuario);
     Optional<Usuario> findById(Long id);
